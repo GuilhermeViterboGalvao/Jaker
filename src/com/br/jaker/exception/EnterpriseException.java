e7148a2f928399fd111563bf4b01af596f9be841
guilherme <catanduva.gvg@gmail.com>
2013-02-23 15:55:33 -0300

    I forgot to commit this class on last commit

---
+++ src/com/br/jaker/exception/EnterpriseException.java
@@ -0,0 +1,10 @@
+package com.br.jaker.exception;
+
+public class EnterpriseException extends Exception {
+
+	private static final long serialVersionUID = -5423867781964603778L;
+	
+	public EnterpriseException(String message) {
+		super(message);
+	}
+}
