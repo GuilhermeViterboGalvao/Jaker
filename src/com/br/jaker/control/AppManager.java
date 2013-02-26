579d4891e2bad8304be28938bfcb93589dbbac4a
GuilhermeViterboGalvao <catanduva.gvg@gmail.com>
2013-02-25 21:17:10 -0300

    Inserted the httpmime lib, and start the class AppManager and JakerApp.

--- src/com/br/jaker/control/AppManager.java
+++ src/com/br/jaker/control/AppManager.java
@@ -1,27 +1,194 @@
 package com.br.jaker.control;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.AbstractHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+
 import android.content.res.AssetManager;
+import android.util.Log;
+
 import com.br.jaker.exception.BookExpection;
+import com.br.jaker.exception.EditionException;
 import com.br.jaker.exception.EnterpriseException;
 import com.br.jaker.model.Book;
+import com.br.jaker.model.Edition;
 import com.br.jaker.model.Enterprise;
 
 public class AppManager {
 	
+	private HttpGet requestGet;
+	
+	private HttpPost requestPost;
+	
+	private HttpResponse response;
+	
+	private HttpClient httpClient;
+	
 	private Enterprise enterprise;
 	
-	private Book book;
+	private List<Edition> editions;
 	
-	public AppManager(AssetManager assetManager) throws EnterpriseException, IOException {
+	private List<Book> books;
+	
+	private File rootPath;	
+	
+	public AppManager(HttpClient httpClient, AssetManager assetManager, File rootPath) throws EnterpriseException, IOException, EditionException, BookExpection {
+		this.httpClient = httpClient;
 		enterprise = SAXEnterpriseParser.parseEnterprise(assetManager.open("Enterprise.xml"));
+		
+		if (enterprise != null) {
+			
+			editions = SAXEditionsParser.parseEdition(doGet(enterprise.getUrlJsonEdtions()));
+			books = new ArrayList<Book>(editions.size());
+			
+			this.rootPath = new File(rootPath, "jaker/" + enterprise.getName());
+			if (!this.rootPath.exists()) this.rootPath.mkdirs();
+			
+			File[] booksPaths = this.rootPath.listFiles();
+			for (File bookPath : booksPaths) {
+				if (bookPath.isDirectory()) {
+					File bookJson = new File(bookPath, "book.json");
+					if (bookJson.exists() && bookJson.isFile()) {
+						books.add(JSONBookParser.parseBook(new FileInputStream(bookJson)));
+					}
+				}
+			}
+			
+			//Verificar no diretório pricipal/nomedarevista os arquivos que lá tem.
+			//Depois baixar os que são novos.
+		}
 	}
 	
 	public Book getBook(InputStream jsonBook) throws BookExpection {
 		return JSONBookParser.parseBook(jsonBook);
 	}
 	
+	public synchronized InputStream doGet(String url) {
+		Log.i("AppManager.doGet", "url: " + url);
+		
+		requestGet = new HttpGet(url);
+		requestGet.setHeader("Accept", "text/plain");
+		
+		try {
+			response = httpClient.execute(requestGet);
+		} catch (ClientProtocolException cpe) {
+			Log.i("AppManager.doGet", "Execution stoped: " + cpe.getMessage());
+			return null;
+		} catch (IOException ioe) {
+			Log.i("AppManager.doGet", "Execution stoped: " + ioe.getMessage());
+			return null;
+		} catch (Exception e) {
+			Log.i("AppManager.doGet", "Execution stoped: " + e.getMessage());
+			return null;
+		}
+		
+		int statusCode = response.getStatusLine().getStatusCode();
+		
+		if (statusCode != HttpStatus.SC_OK) {
+			try {
+				response.getEntity().getContent().close();
+			} catch (IOException ioe) {
+				Log.i("AppManager.doGet", "Error on close InputStream (response.getEntity().getContent().close()): " + ioe.getMessage());
+			} catch (Exception e) {
+				Log.i("AppManager.doGet", "Error on close InputStream (response.getEntity().getContent().close()): " + e.getMessage());
+			}
+			return null;
+		}
+		
+		InputStream in = null;
+		
+		try {
+			in = response.getEntity().getContent();
+		} catch (IllegalStateException ise) {
+			in = null;
+			Log.e("AppManager.doGet", "Error on read InputStream (response.getEntity().getContent()): " + ise.getMessage(), ise);
+		} catch (IOException ioe) {
+			in = null;
+			Log.e("AppManager.doGet", "Error on read InputStream (response.getEntity().getContent()): " + ioe.getMessage(), ioe);
+		} catch (Exception e) {
+			in = null;
+			Log.e("AppManager.doGet", "Error on read InputStream (response.getEntity().getContent()): " + e.getMessage(), e);
+		}
+		
+		return in;
+	}
 	
-	//Criar o JSON parser de edtions.
+	public synchronized InputStream doPost(String url, String[] names, String[] values) {
+		Log.i("AppManager.doPost", "url: " + url);
+		
+		requestPost = new HttpPost(url);
+		requestPost.setHeader("Accept", "text/plain");
+		
+		InputStream in = null;
+		
+		if (values != null && names != null && values.length == names.length) {
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(values.length);
+			
+			for (int i = 0; i < names.length; i++) {
+				nameValuePairs.add(new BasicNameValuePair(names[i], values[i]));		
+			}
+			
+	        try {
+				requestPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			} catch (UnsupportedEncodingException usfee) {
+				Log.e("AppManager.doPost", "Error on 'requestPost.setEntity(new UrlEncodedFormEntity(nameValuePairs))': " + usfee.getMessage(), usfee);
+			} catch (Exception e) {
+				Log.e("AppManager.doPost", "Error on 'requestPost.setEntity(new UrlEncodedFormEntity(nameValuePairs))': " + e.getMessage(), e);
+			}
+	        
+			try {
+				response = httpClient.execute(requestPost);
+			} catch (ClientProtocolException cpe) {
+				Log.i("AppManager.doPost", "Execution stoped: " + cpe.getMessage());
+			} catch (IOException ioe) {
+				Log.i("AppManager.doPost", "Execution stoped: " + ioe.getMessage());
+			} catch (Exception e) {
+				Log.i("AppManager.doPost", "Execution stoped: " + e.getMessage());
+			}
+			
+			int statusCode = response.getStatusLine().getStatusCode();
+			
+			if (statusCode != HttpStatus.SC_OK) {
+				try {
+					response.getEntity().getContent().close();
+				} catch (IOException ioe) {
+					Log.i("AppManager.doPost", "Error on close InputStream (response.getEntity().getContent().close()): " + ioe.getMessage());
+				} catch (Exception e) {
+					Log.i("AppManager.doPost", "Error on close InputStream (response.getEntity().getContent().close()): " + e.getMessage());
+				}
+				return null;
+			}
+			
+			try {
+				in = response.getEntity().getContent();
+			} catch (IllegalStateException ise) {
+				in = null;
+				Log.e("AppManager.doPost", "Error on 'response.getEntity().getContent()': " + ise.getMessage(), ise);
+			} catch (IOException ioe) {
+				in = null;
+				Log.e("AppManager.doPost", "Error on 'response.getEntity().getContent()': " + ioe.getMessage(), ioe);
+			} catch (Exception e) {
+				in = null;
+				Log.e("AppManager.doPost", "Error on 'response.getEntity().getContent()': " + e.getMessage(), e);
+			}
+		}
+		
+		return in;
+	}
 }
--- src/com/br/jaker/view/JakerApp.java
+++ src/com/br/jaker/view/JakerApp.java
@@ -3,13 +3,63 @@ package com.br.jaker.view;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.http.NoHttpResponseException;
+import org.apache.http.client.HttpRequestRetryHandler;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.AbstractHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+
 import com.br.jaker.control.AppManager;
+import com.br.jaker.exception.EditionException;
 import com.br.jaker.exception.EnterpriseException;
 import com.br.jaker.model.Book;
 import android.app.Application;
+import android.os.Environment;
+import android.util.Log;
 
 public class JakerApp extends Application {
 
+	private static final HttpRequestRetryHandler retryHandler;
+	
+	private static final AbstractHttpClient httpClient;
+	
+	static {
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));		
+		HttpParams clientParams = new BasicHttpParams();
+		HttpConnectionParams.setConnectionTimeout(clientParams, 10 * 1000);
+		HttpConnectionParams.setSoTimeout(clientParams, 10 * 1000);
+		ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(clientParams, schemeRegistry);	
+		httpClient = new DefaultHttpClient(cm, null);
+		httpClient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "Jaker App 1.0");
+		retryHandler = new DefaultHttpRequestRetryHandler(5, false) {
+			@Override
+			public boolean retryRequest(IOException exception, int executionCount, HttpContext context) {
+				Log.i("http-exception", exception.getClass().getSimpleName() + " : " + exception.getMessage());
+				
+				boolean result = super.retryRequest(exception, executionCount, context);
+				
+				if (result == false) return false;
+				
+				if (exception instanceof NoHttpResponseException) return true;
+				
+				try { Thread.sleep(2000); } catch (InterruptedException e) { }
+				
+				return true;
+			}
+		};
+		httpClient.setHttpRequestRetryHandler(retryHandler);
+	}
+	
 	private AppManager appManager;
 	
 	public AppManager getAppManager() {
@@ -24,12 +74,17 @@ public class JakerApp extends Application {
 	
 	@Override
 	public void onCreate() {
-		super.onCreate();		
+		super.onCreate();
+		
 		try {
-			appManager = new AppManager(getAssets());
+			appManager = new AppManager(httpClient, getAssets(), Environment.getExternalStorageDirectory());
 		} catch (EnterpriseException e) {
-
+			
+		} catch (EditionException e) {
+			
 		} catch (IOException e) {
+
+		} catch (Exception e) {
 
 		}
 	}
