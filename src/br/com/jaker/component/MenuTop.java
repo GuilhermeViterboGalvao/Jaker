46bd389bda9c339023464132a0defa509cffa2a5
guilherme <guilherme@waves.com.br>
2013-04-24 15:41:54 -0300

    TODOs

--- src/br/com/jaker/component/MenuTop.java
+++ src/br/com/jaker/component/MenuTop.java
@@ -1,14 +1,20 @@
 package br.com.jaker.component;
 
 import br.com.jaker.view.R;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
-public class MenuTop extends RelativeLayout implements OnClickListener {
+@SuppressLint("NewApi")
+public class MenuTop extends RelativeLayout implements OnClickListener, OnTouchListener {
 	
 	private Button btnMenu;
 	
@@ -18,27 +24,33 @@ public class MenuTop extends RelativeLayout implements OnClickListener {
 	
 	private OnSlide onSlide;
 	
+	private ViewPager viewPager;
+	
 	private Context context;
+	
+	private int currentX = 0;
+	
+	private boolean isSmoothingScrool = false;
 	
 	public MenuTop(Context context) {
 		super(context);
 		this.context = context;
-		init();
+		init(0);
 	}
 	
 	public MenuTop(Context context, AttributeSet attributs){
 		super(context, attributs);
 		this.context = context;
-		init();
+		init(attributs.getAttributeIntValue("viewPagerId", "viewPagerId", 0));
 	}
 	
 	public MenuTop(Context context, AttributeSet attributs, int defStyle){
 		super(context, attributs, defStyle);
 		this.context = context;
-		init();
+		init(attributs.getAttributeIntValue("viewPagerId", "viewPagerId", 0));
 	}
 	
-	private void init() {
+	private void init(int idViewPager) {
 		btnMenu = new Button(context);
 		btnMenu.setOnClickListener(this);
 		btnMenu.setWidth(42);
@@ -54,18 +66,24 @@ public class MenuTop extends RelativeLayout implements OnClickListener {
 		btnSlider.setHeight(42);
 		btnSlider.setText("Deslise seu dedo aqui");
 		btnSlider.setBackgroundColor(0xffffff);
+		btnSlider.setOnTouchListener(this);
 		
+		
+				
 		params = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, 42);
 		params.addRule(RelativeLayout.RIGHT_OF, btnMenu.getId());
 		addView(btnSlider, params);
+		
+		//Log.i("ID", "" + getRootView().getResources().getIdentifier("pager", "sliderPaginator", null));
+		
+		viewPager = (ViewPager)findViewById(R.sliderPaginator.pager);
+		
 	}
 	
 	@Override
 	public void onClick(View v) {
 		if (v.equals(btnMenu) && onMenuButtonClick != null) {
 			onMenuButtonClick.click(btnMenu);
-		} else if (v.equals(btnSlider) && onSlide != null) {
-			onSlide.slide(btnSlider);
 		}
 	}	
 		
@@ -83,5 +101,33 @@ public class MenuTop extends RelativeLayout implements OnClickListener {
 	
 	public interface OnSlide {
 		void slide(View view);
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		switch (event.getAction()) {
+			case MotionEvent.ACTION_MOVE:
+				if (currentX == 0) {
+					currentX = (int)event.getRawX();
+				} else if (currentX < (int)event.getRawX() && !isSmoothingScrool) {
+					//Moving to right
+					viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true);
+					isSmoothingScrool = true;
+					onSlide.slide(v);
+				} else if (currentX > (int)event.getRawX() && !isSmoothingScrool) {
+					//Moving to left
+					viewPager.setCurrentItem(viewPager.getCurrentItem() - 1, true);
+					isSmoothingScrool = true;
+					onSlide.slide(v);
+				}				
+				break;
+				
+			case MotionEvent.ACTION_UP:
+				currentX = 0;
+				isSmoothingScrool = false;
+				break;
+		}
+
+		return true;
 	}
 }
