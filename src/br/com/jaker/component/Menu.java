bbcb607dcf5737f2fd20b551b09e200ba7a53c23
guilherme <guilherme@waves.com.br>
2013-04-30 18:56:06 -0300

    Need fix some problems with the bew component.

---
+++ src/br/com/jaker/component/Menu.java
@@ -0,0 +1,119 @@
+package br.com.jaker.component;
+
+import br.com.jaker.custom.component.JakerFragment;
+import br.com.jaker.custom.component.SlidePageAdapter;
+import br.com.jaker.view.R;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+public class Menu extends RelativeLayout implements OnTouchListener {
+
+	public Menu(Context context) {
+		super(context);
+	}
+	
+	public Menu(Context context, AttributeSet attributs){
+		super(context, attributs);
+	}
+	
+	public Menu(Context context, AttributeSet attributs, int defStyle){
+		super(context, attributs, defStyle);
+	}
+	
+	private FrameLayout frameLayout;
+	
+	private ViewPager viewPager;
+	
+	private Button btnMenu;
+	
+	private Slider slider;
+	
+	private int firstX = 0;
+	
+	@Override
+	protected void onFinishInflate() {
+		btnMenu = new Button(getContext());
+		btnMenu.setWidth(42);
+		btnMenu.setHeight(42);
+		btnMenu.setBackgroundResource(R.drawable.menu);
+		
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(42, 42);		
+		params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);		
+		addView(btnMenu, params);
+		
+		frameLayout = new FrameLayout(getContext());
+		frameLayout.setBackgroundColor(Color.BLACK);
+		frameLayout.setOnTouchListener(this);
+		
+		params = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, 42);
+		params.addRule(RelativeLayout.RIGHT_OF, btnMenu.getId());
+		params.leftMargin = 42;
+		addView(frameLayout, params);
+		
+		slider = new Slider(getContext());
+		slider.setOnTouchListener(this);
+		frameLayout.addView(slider);
+		
+		viewPager = (ViewPager)findViewById(R.sliderPaginator.pager);
+		viewPager.setOnTouchListener(this);
+		params = (LayoutParams)viewPager.getLayoutParams();
+		params.width = RelativeLayout.LayoutParams.FILL_PARENT;
+		params.height = RelativeLayout.LayoutParams.FILL_PARENT;
+		params.topMargin = 42;
+		params.addRule(RelativeLayout.ALIGN_BOTTOM, btnMenu.getId());
+		
+		super.onFinishInflate();
+	}
+
+	@Override
+	public boolean onTouch(View view, MotionEvent event) {
+		Log.i("", "firstX: " + firstX);
+		Log.i("", "currentX: " + resize((int)event.getRawX()));
+		
+		if (view instanceof Slider || view instanceof FrameLayout) {
+			switch (event.getAction()) {
+			
+				case MotionEvent.ACTION_MOVE:
+					viewPager.scrollTo(firstX + resize((int)event.getRawX()), 0);
+					if (firstX == 0) {
+						slider.setVisibility(VISIBLE);
+						firstX = resize((int)event.getRawX());
+					}					
+					break;
+					
+				case MotionEvent.ACTION_UP:
+					Log.i("", "currentItem: " + getCurrentPosition(resize((int)event.getRawX())));
+					viewPager.setCurrentItem(getCurrentPosition(resize((int)event.getRawX())), true);
+					slider.setVisibility(GONE);
+					firstX = 0;
+					break;
+			}
+			slider.onTouchEvent(event);
+		} else if (view instanceof ViewPager) {
+			SlidePageAdapter adapter = (SlidePageAdapter)viewPager.getAdapter();
+			JakerFragment fragment = (JakerFragment)adapter.getItem(viewPager.getCurrentItem());
+			WebView webView = fragment.getWebView();
+			webView.onTouchEvent(event);
+		}
+		
+		return true;
+	}
+	
+	public int resize(int x) {
+		return (x * (viewPager.getWidth() * (viewPager.getCurrentItem() + 1))) / frameLayout.getWidth();
+	}
+	
+	public int getCurrentPosition(int x) {
+		return  (int)Math.floor((x * viewPager.getAdapter().getCount()) / (viewPager.getWidth() * viewPager.getAdapter().getCount()));
+	}
+}
---
+++ src/br/com/jaker/component/Slider.java
@@ -0,0 +1,53 @@
+package br.com.jaker.component;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Slider extends View {
+
+	public Slider(Context context) {
+		super(context);
+		init();
+	}
+	
+	public Slider(Context context, AttributeSet attributs){
+		super(context, attributs);
+		init();
+	}
+	
+	public Slider(Context context, AttributeSet attributs, int defStyle){
+		super(context, attributs, defStyle);
+		init();
+	}
+	
+	private void init() {
+		paint = new Paint();
+		paint.setColor(Color.RED);
+		paint.setStrokeWidth(3);
+	}
+	
+	private Paint paint;
+	
+	private float cx;
+	
+	private float cy;
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawCircle(cx, cy, 10, paint);		
+		canvas.restore();
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		cx = event.getX();
+		cy = event.getY();
+		invalidate();		
+		return true;
+	}
+}
